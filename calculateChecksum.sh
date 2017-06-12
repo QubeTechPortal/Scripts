f34a095d6661a6d7435e374d988e4204d0b4f3c6
AjithPanneerselvam <ajithpanneerselvam@live.com>
2017-06-13 00:12:01 +0530

    script archive

--- calculateChecksum.sh
+++ calculateChecksum.sh
@@ -5,14 +5,14 @@ echo "Enter the archive file name"
 read archiveFileName
 
 mkdir Check
-touch Check/sampleFile.txt
+touch Check/sampleFile
 
 echo "Enter the contents of sample file"
-read contents | cat >> Check/sampleFile.txt
-
+read contents
+echo $contents | cat >> Check/sampleFile
 
 # Create a sample archive file
-tar xvf $archiveFileName sampleFile
+tar cvf $archiveFileName Check/sampleFile
 
 # Print the checksum
 cksum $archiveFileName
