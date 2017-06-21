8a5d0f6f9a25e7a7123f2bd1d53fe1b450e95008
Ajith Panneerselvam <ajithpanneerselvam20@gmail.com>
2017-06-21 17:52:48 +0530

    Update virshInstall.sh

--- virshInstall.sh
+++ virshInstall.sh
@@ -2,13 +2,13 @@
 
 DISK_FORMAT="qcow2" # Define VM disk image format.. qcow2|img
 DISK_PATH="/var/lib/libvirt/images" # Define path where VM disk images are stored
-DISK_SIZE="50" # Define disk size in GB
+DISK_SIZE="100" # Define disk size in GB
 EXTRA_ARGS="console=ttyS0,115200n8 serial"
 LOCATION="http://archive.ubuntu.com/ubuntu/dists/xenial/main/installer-amd64/"
 NETWORK_BRIDGE="br0" # virbr0
 OS_TYPE="linux" # Define OS type to install... linux|windows
 OS_VARIANT="ubuntu16.04" # ubuntu14.04|debian8|centos7.0
-PRESEED_FILE="$DISK_PATH/preseeds/ubuntu1604/preseed.cfg" # Define preseed file for Debian based installs if desired
+PRESEED_FILE="/srv/ftp/preseed.cfg" # Define preseed file for Debian based installs if desired
 PRESEED_INSTALL="true" # Define if preseed install is desired
 RAM="2048" # Define memory to allocate to VM in MB... 512|1024|2048
 VCPUS="2" # Define number of vCPUs to allocate to VM
