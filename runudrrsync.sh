3a6030cc268d8c787c596d0d2362888359ff319c
Ajith Panneerselvam <ajithpanneerselvam20@gmail.com>
2017-06-26 17:17:32 +0530

    Create runudrrsync.sh

---
+++ runudrrsync.sh
@@ -0,0 +1,3 @@
+udr -v -c /remote/server/path/to/udr rsync -avz --delete hostname:/src /dest
+
+# udr -vc /home/itsupport/UDR/src/udr rsync -avc --progress 172.16.2.28:/home/itsupport/source/ /home/itsupport/backup/
