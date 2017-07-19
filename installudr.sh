5e0f4987eff1df643511e975da697df28c0809ff
Arvindthiru <tionarvind@ymail.com>
2017-07-20 01:04:54 +0530

    new install udr

--- installudr.sh
+++ installudr.sh
@@ -1,7 +1,7 @@
-apt-get update
-apt-get install git gcc g++ make libssl-dev incron 
+apt-get install git gcc g++ make libssl-dev incron
 git clone https://github.com/LabAdvComp/UDR.git
 cd UDR
 make -e os=LINUX arch=AMD64
+
 
 
