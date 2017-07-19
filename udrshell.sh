e4a777ab54d727c4f338054a2174daf8cc9ef5d4
Arvindthiru <tionarvind@ymail.com>
2017-07-20 01:07:32 +0530

    shell file for running udr rsync

--- udrshell.sh
+++ udrshell.sh
@@ -7,5 +7,7 @@ read -p "Enter destination ssh port >" dport
 read -p "Enter bandwidth in kbps >" bwd
 read -p "Enter source directory >" sd
 read -p "Enter destination directory >" dd
-
-echo "$spath -c $dpath -P $dport rsync -rvP --size-only --bwlimit=$bwd $sd $dusername@$dipaddress:$dd
+read -p "Enter log file name with path >" log
+touch $log
+echo "$spath -c $dpath -P $dport rsync -rvP --size-only --bwlimit=$bwd $sd $dusername@$dipaddress:$dd >> $log" >> rsync.log
+chmod 775 $log runsync.sh
