ad2ea2b2559a842bf7834e32a9f5624517ecda25
gowtham-kingxo <gowtham.gourov@gmail.com>
2017-07-19 20:54:45 +0530

    Create setStaticIP.sh

---
+++ setStaticIP.sh
@@ -0,0 +1,26 @@
+echo "Enter IP address"
+read ip
+echo "Enter netmask"
+read netmask
+echo "Enter gateway"
+read gateway
+cat > /etc/network/interfaces << EOF
+# This file describes the network interfaces available on your system
+# and how to activate them. For more information, see interfaces(5).
+#helllooooo
+# The loopback network interface
+auto lo
+iface lo inet loopback
+
+# The primary network interface
+auto em1
+iface em1 inet static
+    address $ip
+    netmask $netmask
+    gateway $gateway
+    dns-nameservers 192.168.4.12
+
+EOF
+
+
+reboot
